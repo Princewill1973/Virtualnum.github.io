6960cc1562956de6da372234424c8f93e8289944
Princewill1973 <princewilluzono@gmail.com>
2025-08-23 00:04:05 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const { render } = require('express/lib/response');
 
 const app = express();
 const PORT = process.env.PORT || 10000;
